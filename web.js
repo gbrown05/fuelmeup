f9df7e561d914431e7b45a812529783be94580ea
George Brown <gbrown3593@gmail.com>
2014-04-14 13:23:25 -0400

    working on mongodb

--- web.js
+++ web.js
@@ -1,6 +1,18 @@
 var express = require("express");
 var logfmt = require("logfmt");
+//var routes = require("./routes");
+var http = require("http");
+var path = require ("path");
+var mongo = require("mongodb");
+
 var app = express();
+
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||
+        "mongodb://localhost/fuelmeup";
+
+var db = mongo.Db.connect(mongoUri, function (err, database) {
+      db = database;
+});
 
 app.use(logfmt.requestLogger());
 
