3a2b43be404c8630753fb1664d40e629ec783050
George Brown <gbrown3593@gmail.com>
2014-04-15 02:37:02 -0400

    last commit for the night; added front end inputs, etc

--- frontend/js/carinfo.js
+++ frontend/js/carinfo.js
@@ -26,3 +26,41 @@ $('#carmakes .typeahead').typeahead(null, {
   // is compatible with the typeahead jQuery plugin
   source: carMakes.ttAdapter()
 });
+
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var gases, substringRegex;
+ 
+    // an array that will be populated with substring matches
+    gases = [];
+ 
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+ 
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        // the typeahead jQuery plugin expects suggestions to a
+        // JavaScript object, refer to typeahead docs for more info
+        gases.push({ value: str });
+      }
+    });
+ 
+    cb(gases);
+  };
+};
+ 
+var types = ['Premium', 'Plus', 'Regular', 'Diesel', 'Jet Fuel'
+];
+ 
+$('#typegas .typeahead').typeahead({
+  hint: true,
+  highlight: true,
+  minLength: 1
+},
+{
+  name: 'types',
+  displayKey: 'value',
+  source: substringMatcher(types)
+});
