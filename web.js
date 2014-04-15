9fabf58d85476fa7ddd99f2548c7bcf9b6f736e0
George Brown <gbrown3593@gmail.com>
2014-04-15 17:44:37 -0400

    starting on API; also small frontend changes

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var sanitizeInput = require("validator").sanitize;
 //var routes = require("./routes");
 var http = require("http");
 var path = require ("path");
@@ -20,6 +21,10 @@ app.use(express.static(path.join(__dirname, "frontend")));
 app.get('/', function(req, res) {
         res.sendfile(__dirname, 'frontend', path.basename("index.html"));
 });
+
+//app.get('/cars1.json', function(req, res) {
+//        var carMake = req.query.make;
+
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
