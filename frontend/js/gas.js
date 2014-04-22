735e62cf1eac5225ce5cc4490038841969bbda86
Victor Chao <chia-chi.chao@tufts.edu>
2014-04-21 23:22:31 -0400

    created gas.js

---
+++ frontend/js/gas.js
@@ -0,0 +1,44 @@
+/*
+This function computes the actual price of gas given the following parameters:
+d1 = distance from origin point to gas station
+d2 = distance from gas station to destination point
+tc = tank capacity of car being used.
+f = fraction of fuel tank to fill
+mpg = miles per gallon of user car
+retail = price per gallon of gas at a specific gas station in dollars per gallon.
+*/
+var carMake;
+var carModelYear;
+var gasType;
+var destination;
+var gasAmount;
+var distance;
+
+function actualPrice (retail, d1, d2, tc, f, mpg) {
+	var w = retail((f*tc) + ((d1 + d2)/mpg));
+	return w;
+}
+
+
+function fetchInputs()
+{
+    carMake = document.getElementById("carmake");
+    carModelYear = document.getElementById("carmodelyear");
+    gasType = document.getElementById("gasType");
+    destination = document.getElementById("destination");
+    gasAmount = document.getElementById("how much");
+    distance = document.getElementById("howfar");
+
+    setLocalStorage();
+}
+
+function setLocalStorage()
+{
+    localStorage["carMake"] = carMake;
+    localStorage["carModelYear"] = carModel;
+    localStorage["gasType"] = gasType;
+    localStorage["destination"] = destination;
+    localStorage["gasAmount"] = gasAmount;
+    localStorage["distance"] = distance;
+}
+
