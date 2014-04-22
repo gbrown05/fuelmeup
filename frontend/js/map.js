3d472cd115b697aea548e9ad92130b3455382b48
Victor Chao <chia-chi.chao@tufts.edu>
2014-04-21 22:49:37 -0400

    combined js files

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -20,6 +20,13 @@ var map;
 var marker;
 var infowindow = new google.maps.InfoWindow();
 
+var carMake;
+var carModelYear;
+var gasType;
+var destination;
+var gasAmount;
+var distance;
+
 function initialize() {
     // Set up the request
     request = new XMLHttpRequest();
@@ -146,4 +153,25 @@ function actualPrice (retail, d1, d2, tc, f, mpg) {
 }
 
 
+function fetchInputs()
+{
+    carMake = document.getElementById("carmake");
+    carModelYear = document.getElementById("carmodelyear");
+    gasType = document.getElementById("gasType");
+    destination = document.getElementById("destination");
+    gasAmount = document.getElementById("how much");
+    distance = document.getElementById("howfar");
+
+    setLocalStorage();
+}
+
+function setLocalStorage()
+{
+    localStorage["carMake"] = carMake;
+    localStorage["carModelYear"] = carModel;
+    localStorage["gasType"] = gasType;
+    localStorage["destination"] = destination;
+    localStorage["gasAmount"] = gasAmount;
+    localStorage["distance"] = distance;
+}
 
